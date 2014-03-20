f6f7f44923c73e6549f77a154c331beee01e974c
Ryan Chapin <rchapin@users.noreply.github.com>
2014-03-19 22:10:53 -0400

    Finished the basic implementation and cleanup.
    . modified:   2column_fullscreen_fixed_sidebar_right/index.html
      . Moved to html5 doctype
      . Set up so that I could load the same page and vi symlinks and php
    include the content on the page for the different scenarios (main
    content, only nav-bar content, etc).
      . Cleaned up some of the css.
    
    . new file:   2column_fullscreen_fixed_sidebar_right/main_content.html
    . new file:   2column_fullscreen_fixed_sidebar_right/nav_bar.html
      . symlinks to index.html
    
    . new file:   2column_fullscreen_fixed_sidebar_right/main_content.php
      . Contains the content for this particular page.

---
+++ 2column_fullscreen_fixed_sidebar_right/main_content.php
@@ -0,0 +1,22 @@
+<?php include( "../nbi_css_includes/css_layouts_preamble.html" ); ?>
+
+                        <hr />
+
+                        <h3>2 Column: Fixed Width with Right Side Bar, Content Area, and Footer that is   Fixed to the Bottom of the Page</h3>
+
+                        <p>A straight-forward CSS layout.  Because of the fixed widths this one doesn't   seem to have problems with any modern browsers.</p>
+
+                        <p>See the CSS markup for more details on this layout.</p>
+
+                        <hr />
+
+<?php // include( "../nbi_css_includes/css_layouts_overview.html" ); ?>
+
+                        <hr />
+
+<?php // include( "../nbi_css_includes/css_layouts_license.html" ); ?>
+
+                        <hr />
+
+<?php // include( "../nbi_css_includes/css_layouts_w3c_buttons.html" ); ?>
+
